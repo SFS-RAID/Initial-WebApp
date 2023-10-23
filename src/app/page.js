f6b889b769cd20f036b97b2b78da12632f7b8d35
SFS-RAID <99122481+SFS-RAID@users.noreply.github.com>
2023-10-23 03:34:36 +0000

    Fix Component

--- src/app/page.js
+++ src/app/page.js
@@ -15,9 +15,9 @@ export default function Home(){
         <div className="container mx-auto mt-20">
             <div className="bg-gray-800 p-8 rounded-lg shadow-md text-white">
                 <div className="text-3xl font-bold mb-4">Welcome to Our Website</div>
+                <div className='text-lg mb-6'>
                 <TitleComponent title='ABC' subtitle='BCS' customClass='font-bold'/>
-                <div className="text-lg mb-6">
-                    Explore the amazing features and content we have for you. Join our community and get started today.
+                Explore the amazing features and content we have for you. Join our community and get started today.
                 </div>
                 <a href="#" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
                   Get Started
